bccafae6f77cc0b02526a9f5c309bcd7074fad48
Wenjing Wang <wenjing@squareup.com>
2025-12-22 08:29:36 -0800

    Initial Commit

---
+++ Guitar Beat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Guitar Beat
+//
+//  Created by Wenjing Wang on 12/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Guitar Beat/Guitar_BeatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guitar_BeatApp.swift
+//  Guitar Beat
+//
+//  Created by Wenjing Wang on 12/22/25.
+//
+
+import SwiftUI
+
+@main
+struct Guitar_BeatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
